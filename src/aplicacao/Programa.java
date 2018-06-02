4deab835d5ab3ee978fd7f163c50747144353b10
robsonsilv4 <robsonsilv410@gmail.com>
2018-06-01 22:35:46 -0300

    Arquivos do projeto

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,39 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa(null, "Robson", "robson@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Gabriel", "gabriel@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Gil", "gil@gmail.com");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+
+		// Pessoa p = em.find(Pessoa.class, 2);
+		// System.out.println(p);
+
+		// Pessoa p = em.find(Pessoa.class, 2);
+		// em.getTransaction().begin();
+		// em.remove(p);
+		// em.getTransaction().commit();
+
+		System.out.println("Pronto!");
+
+		em.close();
+		emf.close();
+	}
+
+}
